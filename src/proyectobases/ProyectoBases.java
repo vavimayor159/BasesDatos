bde8c2353266da0d1e9562ae00f6f5ab1d6a62ef
Ivan <>
2013-12-12 23:02:55 -0600

    Agregada la clase para la pantalla principal

--- src/proyectobases/ProyectoBases.java
+++ src/proyectobases/ProyectoBases.java
@@ -5,15 +5,8 @@
 package proyectobases;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -25,21 +18,8 @@ public class ProyectoBases extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.getStyleClass().add("botonServicio");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        BorderPane root = new BorderPane();
-        root.setCenter(btn);
-        AnchorPane botonesPedido = agregarBotonesPedido();
-        root.setBottom(botonesPedido);
-        
-        Scene scene = new Scene(root);
+        PantallaPrincipal primera = new PantallaPrincipal();
+        Scene scene = new Scene(primera.getPrincipal());
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
 
@@ -51,41 +31,6 @@ public class ProyectoBases extends Application {
         primaryStage.setScene(scene);
         scene.getStylesheets().add(ProyectoBases.class.getResource("Principal.css").toExternalForm());
         primaryStage.show();
-    }
-    
-    public HBox botonesServicio() {
-        HBox hbox = new HBox();
-        hbox.setPadding(new Insets(15, 12, 15, 12));
-        hbox.setSpacing(10);
-
-        Button botonActual = new Button("Actual");
-        botonActual.getStyleClass().add("botonServicio");
-
-        Button botonSiguiente = new Button("Siguiente");
-        botonSiguiente.getStyleClass().add("botonServicio");
-        hbox.getChildren().addAll(botonActual, botonSiguiente);
-
-        return hbox;
-    }
-    
-    public AnchorPane agregarBotonesPedido() {
-        AnchorPane anchorpane = new AnchorPane();
-        Button botonNuevo = new Button("Nuevo Pedido");
-        botonNuevo.getStyleClass().add("botonCompras");
-        botonNuevo.setPrefSize(150, 100);
-        Button botonCancelar = new Button("Cancel");
-        botonCancelar.getStyleClass().add("botonServicio");
-
-        HBox hb = new HBox();
-        hb.setPadding(new Insets(0, 10, 10, 10));
-        hb.setSpacing(10);
-        hb.getChildren().addAll(botonCancelar, botonNuevo);
-
-        anchorpane.getChildren().addAll(hb);   // Add grid from Example 1-5
-        AnchorPane.setBottomAnchor(hb, 8.0);
-        AnchorPane.setRightAnchor(hb, 5.0);
-
-        return anchorpane;
     }
 
     /**
